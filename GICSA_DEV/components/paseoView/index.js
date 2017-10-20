2ab5f5fe107d43ec6648df7c8ede4af848d59d59
Adrian Ayala <adrian.ayala@bitechnology.com.mx>
2017-10-20 05:20:48 +0000

    Initial

---
+++ GICSA_DEV/components/paseoView/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+app.paseoView = kendo.observable({
+    onShow: function() {},
+    afterShow: function() {}
+});
+app.localization.registerView('paseoView');
+
+// START_CUSTOM_CODE_paseoView
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+
+// END_CUSTOM_CODE_paseoView
---
+++ GICSA_DEV/components/popupView/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+app.popupView = kendo.observable({
+    onShow: function() {},
+    afterShow: function() {}
+});
+app.localization.registerView('popupView');
+
+// START_CUSTOM_CODE_popupView
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+
+// END_CUSTOM_CODE_popupView
