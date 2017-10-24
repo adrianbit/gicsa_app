21bdf328c270280731fb1aa8f0298b2020fa69fa
Adrian Ayala <adrian.ayala@bitechnology.com.mx>
2017-10-24 22:14:09 +0000

    ...

--- GICSA_DEV/components/menuView/index.js
+++ GICSA_DEV/components/menuView/index.js
@@ -5,7 +5,9 @@ var emailNewsletter = "";
 var currentApp = app;
 
 app.menuView = kendo.observable({
-    onShow: function() {},
+    onShow: function() {
+        goToView(getCookie("initial"));
+    },
     afterShow: function() {
     }
 });
@@ -27,7 +29,31 @@ function popUp2(){
     emailNewsletter = $("#emailNewsletter").val();
     if (true || emailNewsletter!=""){
         goToView(selectedPlaza);
+        setCookie("initial",selectedPlaza,30)
     }
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
 function goToView(selectedPlaza){
