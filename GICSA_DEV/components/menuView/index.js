bd304590d1b796e3fe964f325df1d36f78961894
Adrian Ayala <adrian.ayala@bitechnology.com.mx>
2017-10-24 04:23:02 +0000

    ....

--- GICSA_DEV/components/menuView/index.js
+++ GICSA_DEV/components/menuView/index.js
@@ -22,9 +22,19 @@ function popUp1(){
 function popUp2(){
     emailNewsletter = $("#emailNewsletter").val();
     if (emailNewsletter!=""){
-        $("#popUp2").hide();
         $("#headerNavbar").show("");
-        goToView(selectedPlaza);
+        var app = new kendo.mobile.Application();
+        if (selectedPlaza=="forumCuernavaca"){
+            app.navigate("components/forumView/view.html");
+        } else if (selectedPlaza=="islaPuerto"){
+            app.navigate("components/laIslaView/view.html");
+        } else if (selectedPlaza=="paseoArcos"){
+            app.navigate("components/paseoView/view.html");
+        } else if (selectedPlaza=="explanadaCancun"){
+            app.navigate("components/explanadaView/view.html");
+        } else if (selectedPlaza=="plazasOutlet"){
+            app.navigate("components/plazasView/view.html");
+        }
     }
 }
 
